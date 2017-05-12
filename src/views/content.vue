7141a8f610e9d78e0755d4afbd59cfcfe319c785
russellmars <russellmars@sina.com>
2017-05-12 21:33:59 +0800

    内容页

--- src/views/content.vue
+++ src/views/content.vue
@@ -17,15 +17,34 @@
       </header>
       <div class="main">
         <div class="title f18 blue bold caps">{{ title }}</div>
-        <div class="sub-title">
-          <div class="gray-2 f26 bold caps">{{ subTitle.text }}</div>
-          <div class="gray f14 bold  letter-spacing">{{ subTitle.enText }}</div>
-        </div>
-        <div class="group-icons flex justify-between">
-          <div v-for="group in groups">
-            <img :src="group.pic" alt="">
+        <div class="content">
+          <div class="sub-title">
+            <div class="gray-2 f26 bold caps">{{ subTitle.text }}</div>
+            <div class="gray f14 bold letter-spacing">{{ subTitle.enText }}</div>
+          </div>
+          <div class="group-icons flex justify-between gray clearfix bold">
+            <div class="center col col-4" v-for="group in groups">
+              <img class="fit" :src="group.pic" alt="">
+              <div class="letter-spacing">
+                <div class="name f12">{{ group.name }}</div>
+                <div class="en-name f8">{{ group.enName }}</div>
+              </div>
+            </div>
+          </div>
+          <div class="groups gray">
+            <div class="group clearfix" v-for="group in groups">
+              <div class="group-en-name f18 blue bold">{{ group.enName }}</div>
+              <div class="item col md-col-3 sm-col-4 center" v-for="item in group.items">
+                <img class="fit" :src="item.pic" alt="">
+                <div class="letter-spacing">
+                  <div class="name f12 line-height-4">{{ item.name }}</div>
+                  <div class="en-name f8 line-height-4">{{ item.enName }}</div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
+
       </div>
     </div>
   </div>
@@ -58,19 +77,49 @@
         .title {
           margin-top: 120px;
         }
-        .sub-title {
-          margin-top: 160px;
-          margin-left: 20%;
-          & > div {
-            margin: 20px 0;
+        .content {
+          margin: 0 5%;
+          .sub-title {
+            margin-top: 160px;
+            margin-left: 15%;
+            & > div {
+              margin: 20px 0;
+            }
+          }
+          .group-icons {
+            margin-top: 150px;
+            padding: 0 10px;
+            .name {
+              margin: 15px 0 10px 0;
+            }
+            .en-name {
+
+            }
+            img {
+              width: 100%;
+            }
+          }
+          .groups {
+            .group {
+              margin-top: 100px;
+              .group-en-name {
+                margin-bottom: 30px;
+                padding-left: 8%;
+              }
+              .item {
+                padding: 10px;
+                height: 300px;
+                .name {
+                  margin: 10px 0;
+                }
+                .en-name {
+
+                }
+              }
+            }
           }
         }
-        .group-icons {
-          margin-top: 150px;
-          img {
-            width: 100%;
-          }
-        }
+
       }
     }
   }
--- src/views/data/content.js
+++ src/views/data/content.js
@@ -9,121 +9,136 @@ export default {
     pic: 'static/img/content/31.png',
     desc: 'https://russellmars.github.io/lifeplus/#/'
   },
-  nav: [
-    {
-      name: '首页',
-      enName: 'LIFE+',
-      active: false,
-      link: '/'
+  nav: [{
+    name: '首页',
+    enName: 'LIFE+',
+    active: false,
+    link: '/'
+  }, {
+    name: '内容',
+    enName: 'CONTENT',
+    active: true,
+    link: '/content'
+  }, {
+    name: '关于',
+    enName: 'ABOUT',
+    active: false,
+    link: '/about'
+  }],
+  groups: [{
+    pic: 'static/img/content/3.png',
+    name: '衣架',
+    enName: 'HANGERS',
+    items: [{
+      pic: 'static/img/content/6.gif',
+      name: '锅盖支架',
+      enName: 'The pot holder'
     }, {
-      name: '内容',
-      enName: 'CONTENT',
-      active: true,
-      link: '/content'
+      pic: 'static/img/content/7.png',
+      name: '餐具支架',
+      enName: 'Tableware bracket'
     }, {
-      name: '关于',
-      enName: 'ABOUT',
-      active: false,
-      link: '/about'
-    }
-  ],
-  groups: [
-    {
-      pic: 'static/img/content/3.png',
-      name: '衣架',
-      enName: 'HANGERS',
-      items: [
-        {
-          pic: 'static/img/content/6.png',
-          name: '锅盖支架'
-        }, {
-          pic: 'static/img/content/7.png',
-          name: '餐具支架'
-        }, {
-          pic: 'static/img/content/8.png',
-          name: '挂书支架'
-        }, {
-          pic: 'static/img/content/9.png',
-          name: '枕头晾挂架'
-        }, {
-          pic: 'static/img/content/10.png',
-          name: '皮带支架'
-        }, {
-          pic: 'static/img/content/11.png',
-          name: '挂鞋晾挂架'
-        }, {
-          pic: 'static/img/content/12.png',
-          name: '盆栽支架'
-        }, {
-          pic: 'static/img/content/13.png',
-          name: '牙刷支架'
-        }
-      ]
+      pic: 'static/img/content/8.gif',
+      name: '挂书支架',
+      enName: 'Hanging book stand'
     }, {
-      pic: 'static/img/content/4.png',
-      name: '瓶子',
-      enName: 'BOTTLE',
-      items: [
-        {
-          pic: 'static/img/content/14.png',
-          name: '一次性杯套'
-        }, {
-          pic: 'static/img/content/15.png',
-          name: '蜡烛台'
-        }, {
-          pic: 'static/img/content/16.png',
-          name: '排插保护壳'
-        }, {
-          pic: 'static/img/content/17.png',
-          name: '瓶嘴挤压瓶'
-        }, {
-          pic: 'static/img/content/18.png',
-          name: '酱料瓶'
-        }, {
-          pic: 'static/img/content/19.png',
-          name: '水杯'
-        }, {
-          pic: 'static/img/content/20.png',
-          name: '杯子保护套'
-        }, {
-          pic: 'static/img/content/21.png',
-          name: '防潮盖'
-        }, {
-          pic: 'static/img/content/22.png',
-          name: '吸蛋器'
-        }, {
-          pic: 'static/img/content/23.png',
-          name: '花盆'
-        }, {
-          pic: 'static/img/content/24.png',
-          name: '油漆罐'
-        }
-      ]
+      pic: 'static/img/content/9.gif',
+      name: '枕头晾挂架',
+      enName: 'The pot holder'
     }, {
-      pic: 'static/img/content/5.png',
-      name: '其他',
-      enName: 'OTHER',
-      items: [
-        {
-          pic: 'static/img/content/25.png',
-          name: '电脑支架'
-        }, {
-          pic: 'static/img/content/26.png',
-          name: 'ipad支架'
-        }, {
-          pic: 'static/img/content/27.png',
-          name: '手机支架'
-        }, {
-          pic: 'static/img/content/28.png',
-          name: '防滑衣架'
-        }, {
-          pic: 'static/img/content/29.png',
-          name: '挤牙膏'
-        }, {
-          pic: 'static/img/content/30.png',
-          name: '塑料袋收纳'
-        }
-      ]
-    }
-  ]
+      pic: 'static/img/content/10.gif',
+      name: '皮带支架',
+      enName: 'Belt stent'
+    }, {
+      pic: 'static/img/content/11.gif',
+      name: '挂鞋晾挂架',
+      enName: 'GuaXie hang frame'
+    }, {
+      pic: 'static/img/content/12.gif',
+      name: '盆栽支架',
+      enName: 'Potted stents'
+    }, {
+      pic: 'static/img/content/13.png',
+      name: '牙刷支架',
+      enName: 'Toothbrush holder'
+    }]
+  }, {
+    pic: 'static/img/content/4.png',
+    name: '瓶子',
+    enName: 'BOTTLE',
+    items: [{
+      pic: 'static/img/content/14.gif',
+      name: '一次性杯套',
+      enName: 'Disposable cup set'
+    }, {
+      pic: 'static/img/content/15.gif',
+      name: '蜡烛台',
+      enName: 'Candle holders'
+    }, {
+      pic: 'static/img/content/16.gif',
+      name: '排插保护壳',
+      enName: 'Platooninsert protection shell'
+    }, {
+      pic: 'static/img/content/17.gif',
+      name: '瓶嘴挤压瓶',
+      enName: 'The pot holder'
+    }, {
+      pic: 'static/img/content/18.gif',
+      name: '酱料瓶',
+      enName: 'Sauce bottles'
+    }, {
+      pic: 'static/img/content/19.gif',
+      name: '水杯',
+      enName: 'Water glass'
+    }, {
+      pic: 'static/img/content/20.gif',
+      name: '杯子保护套',
+      enName: 'The glass case'
+    }, {
+      pic: 'static/img/content/21.gif',
+      name: '防潮盖',
+      enName: 'The pot holder'
+    }, {
+      pic: 'static/img/content/22.gif',
+      name: '吸蛋器',
+      enName: 'Absorbers egg'
+    }, {
+      pic: 'static/img/content/23.png',
+      name: '花盆',
+      enName: 'A flower pot'
+    }, {
+      pic: 'static/img/content/24.gif',
+      name: '油漆罐',
+      enName: 'Paint cans'
+    }]
+  }, {
+    pic: 'static/img/content/5.png',
+    name: '其他',
+    enName: 'OTHER',
+    items: [{
+      pic: 'static/img/content/25.gif',
+      name: '电脑支架',
+      enName: 'Computer support'
+    }, {
+      pic: 'static/img/content/26.gif',
+      name: 'ipad支架',
+      enName: 'ipad stents'
+    }, {
+      pic: 'static/img/content/27.png',
+      name: '手机支架',
+      enName: 'Mobile phone stents'
+    }, {
+      pic: 'static/img/content/28.gif',
+      name: '防滑衣架',
+      enName: 'Non-slip hanger'
+    }, {
+      pic: 'static/img/content/29.png',
+      name: '挤牙膏',
+      enName: 'toothpaste'
+    }, {
+      pic: 'static/img/content/30.gif',
+      name: '塑料袋收纳',
+      enName: 'Plastic bags to receive'
+    }]
+  }]
 }
