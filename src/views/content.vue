2fbc0c1fd6702be5cb188a3d25b2ec3faf9bf57a
russellmars <russellmars@sina.com>
2017-05-15 20:27:41 +0800

    content 交互

--- src/views/content.vue
+++ src/views/content.vue
@@ -23,7 +23,7 @@
             <div class="gray f14 bold letter-spacing">{{ subTitle.enText }}</div>
           </div>
           <div class="group-icons flex justify-between gray clearfix bold">
-            <div class="center col col-4" v-for="group in groups">
+            <div class="group-icon center col col-4 pointer" v-for="group in groups" @click="scrollTargetGroup(group)">
               <img class="fit" :src="group.pic" alt="">
               <div class="letter-spacing">
                 <div class="name f12">{{ group.name }}</div>
@@ -32,30 +32,37 @@
             </div>
           </div>
           <div class="groups gray">
-            <div class="group clearfix" v-for="group in groups">
+            <div class="group clearfix" v-for="group in groups" :id="'group_' + group.enName">
               <div class="group-en-name f18 blue bold">{{ group.enName }}</div>
-              <div class="item col md-col-3 sm-col-4 center" v-for="item in group.items">
-                <img class="fit" :src="item.pic" alt="">
-                <div class="letter-spacing">
-                  <div class="name f12 line-height-4">{{ item.name }}</div>
-                  <div class="en-name f8 line-height-4">{{ item.enName }}</div>
+              <div class="item col col-6 sm-col-4 md-col-3 center pointer"
+                v-for="item in group.items"
+                @click="checkDetail(item)"
+                @mouseleave="mouseleaveItem(item)"
+                @mouseenter="mouseenterItem(item)">
+                <div class="item-wrapper relative">
+                  <img class="fit" :src="item.pic" alt="" :style="{opacity: item.hover ? 0 : 1}">
+                  <div class="absolute top-0 left-0 top-0 bottom-0 right-0"><img class="fit" :src="item.gif" alt="" style="width:0;height:0;"><img class="fit" :src="item.gif" v-if="item.hover" alt=""></div>
+                  <div class="letter-spacing">
+                    <div class="name f12 line-height-4">{{ item.name }}</div>
+                    <div class="en-name f8 line-height-4">{{ item.enName }}</div>
+                  </div>
+
                 </div>
               </div>
             </div>
           </div>
         </div>
-
       </div>
     </div>
   </div>
 </template>
 
 <style lang="less" scoped>
+  @import "../style/var.less";
   .root {
     margin: 0 auto;
     .container {
       max-width: 1200px;
-      min-width: 600px;
       height: 400px;
       padding: 0 10%;
       margin: 0 auto;
@@ -89,14 +96,26 @@
           .group-icons {
             margin-top: 150px;
             padding: 0 10px;
-            .name {
-              margin: 15px 0 10px 0;
-            }
-            .en-name {
+            .group-icon {
+              padding: 40px 10px 20px 10px;
+              border-left: 1px solid @color-grey-2;
+              &:last-of-type {
+                border-right: 1px solid @color-grey-2;
+              }
 
-            }
-            img {
-              width: 100%;
+              .name {
+                margin: 15px 0 10px 0;
+              }
+              .en-name {
+
+              }
+              img {
+                width: 100%;
+                &:hover {
+                  transform: scale(1.25, 1.25);
+                  transition: transform .5s;
+                }
+              }
             }
           }
           .groups {
@@ -109,6 +128,10 @@
               .item {
                 padding: 10px;
                 height: 300px;
+                .item-wrapper {
+                  width: 100%;
+                  height: 100%;
+                }
                 .name {
                   margin: 10px 0;
                 }
@@ -128,9 +151,28 @@
 
 <script>
   import contentData from './data/content'
+  import Velocity from 'velocity-animate'
   export default {
     data () {
       return contentData
+    },
+    methods: {
+      scrollTargetGroup (group) {
+        Velocity(
+          document.getElementById('group_' + group.enName),
+          'scroll',
+          { duration: 1000 }
+        )
+      },
+      checkDetail (item) {
+        console.log('checkDetail', item)
+      },
+      mouseleaveItem (item) {
+        item.hover = false
+      },
+      mouseenterItem (item) {
+        item.hover = true
+      }
     }
   }
 </script>
--- src/views/data/content.js
+++ src/views/data/content.js
@@ -30,35 +30,51 @@ export default {
     name: '衣架',
     enName: 'HANGERS',
     items: [{
-      pic: 'static/img/content/6.gif',
+      pic: 'static/img/content/6.jpg',
+      gif: 'static/img/content/6.gif',
+      hover: false,
       name: '锅盖支架',
       enName: 'The pot holder'
     }, {
       pic: 'static/img/content/7.png',
+      gif: 'static/img/content/7.png',
+      hover: false,
       name: '餐具支架',
       enName: 'Tableware bracket'
     }, {
-      pic: 'static/img/content/8.gif',
+      pic: 'static/img/content/8.jpg',
+      gif: 'static/img/content/8.gif',
+      hover: false,
       name: '挂书支架',
       enName: 'Hanging book stand'
     }, {
-      pic: 'static/img/content/9.gif',
+      pic: 'static/img/content/9.jpg',
+      gif: 'static/img/content/9.gif',
+      hover: false,
       name: '枕头晾挂架',
       enName: 'The pot holder'
     }, {
-      pic: 'static/img/content/10.gif',
+      pic: 'static/img/content/10.jpg',
+      gif: 'static/img/content/10.gif',
+      hover: false,
       name: '皮带支架',
       enName: 'Belt stent'
     }, {
-      pic: 'static/img/content/11.gif',
+      pic: 'static/img/content/11.jpg',
+      gif: 'static/img/content/11.gif',
+      hover: false,
       name: '挂鞋晾挂架',
       enName: 'GuaXie hang frame'
     }, {
-      pic: 'static/img/content/12.gif',
+      pic: 'static/img/content/12.jpg',
+      gif: 'static/img/content/12.gif',
+      hover: false,
       name: '盆栽支架',
       enName: 'Potted stents'
     }, {
-      pic: 'static/img/content/13.png',
+      pic: 'static/img/content/13.jpg',
+      gif: 'static/img/content/13.gif',
+      hover: false,
       name: '牙刷支架',
       enName: 'Toothbrush holder'
     }]
@@ -67,47 +83,69 @@ export default {
     name: '瓶子',
     enName: 'BOTTLE',
     items: [{
-      pic: 'static/img/content/14.gif',
+      pic: 'static/img/content/14.jpg',
+      gif: 'static/img/content/14.gif',
+      hover: false,
       name: '一次性杯套',
       enName: 'Disposable cup set'
     }, {
-      pic: 'static/img/content/15.gif',
+      pic: 'static/img/content/15.jpg',
+      gif: 'static/img/content/15.gif',
+      hover: false,
       name: '蜡烛台',
       enName: 'Candle holders'
     }, {
-      pic: 'static/img/content/16.gif',
+      pic: 'static/img/content/16.jpg',
+      gif: 'static/img/content/16.gif',
+      hover: false,
       name: '排插保护壳',
       enName: 'Platooninsert protection shell'
     }, {
-      pic: 'static/img/content/17.gif',
+      pic: 'static/img/content/17.jpg',
+      gif: 'static/img/content/17.gif',
+      hover: false,
       name: '瓶嘴挤压瓶',
       enName: 'The pot holder'
     }, {
-      pic: 'static/img/content/18.gif',
+      pic: 'static/img/content/18.jpg',
+      gif: 'static/img/content/18.gif',
+      hover: false,
       name: '酱料瓶',
       enName: 'Sauce bottles'
     }, {
-      pic: 'static/img/content/19.gif',
+      pic: 'static/img/content/19.jpg',
+      gif: 'static/img/content/19.gif',
+      hover: false,
       name: '水杯',
       enName: 'Water glass'
     }, {
-      pic: 'static/img/content/20.gif',
+      pic: 'static/img/content/20.jpg',
+      gif: 'static/img/content/20.gif',
+      hover: false,
       name: '杯子保护套',
       enName: 'The glass case'
     }, {
-      pic: 'static/img/content/21.gif',
+      pic: 'static/img/content/21.jpg',
+      gif: 'static/img/content/21.gif',
+      hover: false,
       name: '防潮盖',
       enName: 'The pot holder'
     }, {
-      pic: 'static/img/content/22.gif',
+      pic: 'static/img/content/22.jpg',
+      gif: 'static/img/content/22.gif',
+      hover: false,
       name: '吸蛋器',
       enName: 'Absorbers egg'
     }, {
       pic: 'static/img/content/23.png',
+      gif: 'static/img/content/23.gif',
+      hover: false,
       name: '花盆',
       enName: 'A flower pot'
     }, {
-      pic: 'static/img/content/24.gif',
+      pic: 'static/img/content/24.jpg',
+      gif: 'static/img/content/24.gif',
+      hover: false,
       name: '油漆罐',
       enName: 'Paint cans'
     }]
@@ -116,27 +154,39 @@ export default {
     name: '其他',
     enName: 'OTHER',
     items: [{
-      pic: 'static/img/content/25.gif',
+      pic: 'static/img/content/25.jpg',
+      gif: 'static/img/content/25.gif',
+      hover: false,
       name: '电脑支架',
       enName: 'Computer support'
     }, {
-      pic: 'static/img/content/26.gif',
+      pic: 'static/img/content/26.jpg',
+      gif: 'static/img/content/26.gif',
+      hover: false,
       name: 'ipad支架',
       enName: 'ipad stents'
     }, {
       pic: 'static/img/content/27.png',
+      gif: 'static/img/content/27.gif',
+      hover: false,
       name: '手机支架',
       enName: 'Mobile phone stents'
     }, {
-      pic: 'static/img/content/28.gif',
+      pic: 'static/img/content/28.jpg',
+      gif: 'static/img/content/28.gif',
+      hover: false,
       name: '防滑衣架',
       enName: 'Non-slip hanger'
     }, {
-      pic: 'static/img/content/29.png',
+      pic: 'static/img/content/29.jpg',
+      gif: 'static/img/content/29.gif',
+      hover: false,
       name: '挤牙膏',
       enName: 'toothpaste'
     }, {
-      pic: 'static/img/content/30.gif',
+      pic: 'static/img/content/30.jpg',
+      gif: 'static/img/content/30.gif',
+      hover: false,
       name: '塑料袋收纳',
       enName: 'Plastic bags to receive'
     }]
